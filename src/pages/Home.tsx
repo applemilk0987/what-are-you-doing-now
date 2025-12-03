15c78edb7ade5b500f5c1c2775c2a407df6ad0d3
applemilk0987 <applemilk0987@gmail.com>
2025-12-03 17:15:09 +0900

    [25-12-03] 홈화면 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-	return (
-		<div>
-			<h1>Welcome to "What Are You Doing Now?"</h1>
-			<p>Start logging your activities!</p>
-			<Link to="/log">Log Activity</Link>
-			<br />
-			<Link to="/history">View History</Link>
-		</div>
-	);
+  return (
+    <div className="home-container">
+      <h1>Welcome to "What Are You Doing Now?"</h1>
+      <p>Start logging your activities!</p>
+      <div className="home-buttons">
+        <Link to="/log" className="btn">Log Activity</Link>
+        <Link to="/history" className="btn">View History</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
